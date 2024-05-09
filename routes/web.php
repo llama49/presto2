a46e11ab872eda1a12794675e6e2cc2c84a570d4
Antonio <antoniominafra.dev@gmail.com>
2024-05-09 17:00:30 +0200

    salvato web.php

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PublicController::class , 'homepage'])->name('welcome');
 
 Route::get('create/announcements', [AnnouncementsController::class , 'createAnnouncements'])->name('create.announcements')->middleware('auth');
-
