51f8b74188b83bcae115cfa753d0ef937434bc1a
Giorgio <vaccarino.giorgio@gmail.com>
2024-05-10 18:07:52 +0200

    aggiustate categorie seeder

--- database/seeders/AnnouncementSeeder.php
+++ database/seeders/AnnouncementSeeder.php
@@ -19,7 +19,7 @@ class AnnouncementSeeder extends Seeder
                 'body' => 'Una bellissima moto in ottime condizioni. Ideale per gli amanti delle due ruote, con un motore potente e un design accattivante. Perfetta per viaggiare in città o per lunghe escursioni su strade panoramiche.',
                 'price' => '12',
                 'user_id' => '1',
-                'category_id' => '1',
+                'category_id' => '2',
                 'img' => 'public/img/default-image.jpg'
             ],
             [
@@ -35,7 +35,7 @@ class AnnouncementSeeder extends Seeder
                 'body' => 'Una bicicletta da corsa professionale, progettata per prestazioni eccezionali su strada. Con un telaio leggero e resistente, questa bici ti porterà ovunque tu voglia andare. Perfetta per gli allenamenti intensi o per gareggiare con gli amici.',
                 'price' => '20',
                 'user_id' => '3',
-                'category_id' => '3',
+                'category_id' => '6',
                 'img' => 'public/img/default-image.jpg'
             ],
             [
@@ -43,7 +43,7 @@ class AnnouncementSeeder extends Seeder
                 'body' => 'Un potente computer per gaming, progettato per offrire prestazioni incredibili e un\'esperienza di gioco immersiva. Con una scheda grafica avanzata e un processore veloce, questo PC è pronto a gestire anche i giochi più esigenti. Goditi la massima potenza e la massima precisione con questo computer di alta qualità.',
                 'price' => '1000',
                 'user_id' => '4',
-                'category_id' => '4',
+                'category_id' => '3',
                 'img' => 'public/img/default-image.jpg'
             ],
             [
@@ -51,7 +51,7 @@ class AnnouncementSeeder extends Seeder
                 'body' => 'Un libro di fantascienza avvincente che ti porterà in mondi lontani e avventure incredibili. Scritto da uno dei migliori autori del genere, questo libro ti terrà incollato alle pagine fino all\'ultima riga. Perfetto per chi ama lasciarsi trasportare dalla fantasia e dalla creatività.',
                 'price' => '10',
                 'user_id' => '1',
-                'category_id' => '5',
+                'category_id' => '6',
                 'img' => 'public/img/default-image.jpg'
             ],
             [
@@ -59,7 +59,7 @@ class AnnouncementSeeder extends Seeder
                 'body' => 'Uno smartphone con fotocamera avanzata e prestazioni straordinarie. Con un design elegante e uno schermo luminoso, questo telefono è perfetto per catturare i momenti speciali e restare connessi con chi ami. Goditi un\'esperienza mobile senza pari con questo smartphone di alta gamma.',
                 'price' => '500',
                 'user_id' => '2',
-                'category_id' => '6',
+                'category_id' => '3',
                 'img' => 'public/img/default-image.jpg'
             ],
             [
@@ -67,7 +67,7 @@ class AnnouncementSeeder extends Seeder
                 'body' => 'Una casa con vista mare, situata in una delle zone più esclusive della città. Con ampi spazi interni ed esterni, questa casa è perfetta per chi ama vivere all\'aria aperta e godersi la bellezza del mare. Confortevole, elegante e moderna, questa casa è pronta ad accogliere te e la tua famiglia.',
                 'price' => '200000',
                 'user_id' => '3',
-                'category_id' => '7',
+                'category_id' => '1',
                 'img' => 'public/img/default-image.jpg'
             ],
             [
@@ -75,7 +75,7 @@ class AnnouncementSeeder extends Seeder
                 'body' => 'Una giacca di pelle di alta qualità, perfetta per aggiungere un tocco di stile e eleganza al tuo guardaroba. Realizzata con materiali pregiati e lavorata con cura artigianale, questa giacca ti terrà al caldo e ti farà sentire al meglio in ogni occasione. Indossa la tua giacca con orgoglio e stile.',
                 'price' => '150',
                 'user_id' => '4',
-                'category_id' => '8',
+                'category_id' => '7',
                 'img' => 'public/img/default-image.jpg'
             ],
             [
@@ -83,7 +83,7 @@ class AnnouncementSeeder extends Seeder
                 'body' => 'Un orologio da polso elegante e raffinato, perfetto per completare il tuo look in ogni occasione. Con un design classico e una meccanica di precisione, questo orologio ti accompagnerà ovunque tu vada. Aggiungi un tocco di lusso al tuo polso con questo orologio di alta qualità.',
                 'price' => '300',
                 'user_id' => '1',
-                'category_id' => '9',
+                'category_id' => '7',
                 'img' => 'public/img/default-image.jpg'
             ],
             [
@@ -91,7 +91,7 @@ class AnnouncementSeeder extends Seeder
                 'body' => 'Un paio di scarpe da corsa performanti, progettate per offrire comfort e supporto durante ogni passo. Con una suola ammortizzata e un design leggero, queste scarpe ti permetteranno di correre più veloce e più a lungo. Affronta ogni sfida con fiducia e stile con queste scarpe da corsa di alta qualità.',
                 'price' => '80',
                 'user_id' => '2',
-                'category_id' => '10',
+                'category_id' => '7',
                 'img' => 'public/img/default-image.jpg'
             ]
         ];
