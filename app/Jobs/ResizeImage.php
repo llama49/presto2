f5a839cc56e9923a7608583d9e558c3e8913ca24
emanuele-04 <emanuelepelliccia2004@gmail.com>
2024-05-22 12:23:05 +0200

    aggiunto oscuramento volti, watermark automatico al caricamento delle foto

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -3,8 +3,11 @@
 namespace App\Jobs;
 
 use Spatie\Image\Image;
+use Spatie\Image\Enums\Fit;
+use Spatie\Image\Enums\Unit;
 use Illuminate\Bus\Queueable;
 use Spatie\Image\Enums\CropPosition;
+use Spatie\Image\Enums\AlignPosition;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -41,7 +44,16 @@ class ResizeImage implements ShouldQueue
         $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
 
         $croppedImage = Image::load($srcPath)
-                        ->crop($w , $h, CropPosition::Center)
+                      ->fit(Fit::Crop, 300, 300)
+                        ->watermark(
+                            base_path('resources/img/watermarkFoto.png'),
+                            width: 70,
+                            height: 70,
+                            alpha: 50,
+                            paddingX: 5,
+                            paddingY: 5,
+                            paddingUnit: Unit::Percent,
+                        )
                         ->save($destPath);
 
     }
--- app/Livewire/CreateAnnouncements.php
+++ app/Livewire/CreateAnnouncements.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Image;
 use Livewire\Component;
 use App\Models\Category;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use App\Models\Announcement;
 use App\Models\Announcements;
@@ -20,42 +21,42 @@ class CreateAnnouncements extends Component
     #[Validate('required', message: 'ui.required')]
     #[Validate('min:3', message: 'ui.required2')]
     public $title;
-
+    
     #[Validate('required', message: 'ui.required')]
     public $body;
-
+    
     #[Validate('required', message: 'ui.required')]
     public $price;
-
+    
     #[Validate('required', message: 'ui.required')]
     public $category;
-
+    
     #[Validate(['images.*' => 'image|max:1024'])]
     public $images = [];
-
+    
     #[Validate(['temporary_images.*' => 'image|max:1024'])]
     public $temporary_images;
-
+    
     public $announcement;
-
+    
     public $newImage;
-
-
+    
+    
     use WithFileUploads;
-
+    
     public function updatedTemporaryImages()
     {
         foreach ($this->temporary_images as $image) {
-
+            
             $this->images[] = $image;
-
+            
         }
     }
     public function store(){
-
+        
         $this->validate();
-
-
+        
+        
         $announcement = Announcement::create([
             'title'=>$this->title,
             'body'=>$this->body,
@@ -63,59 +64,60 @@ class CreateAnnouncements extends Component
             'user_id' => Auth::user()->id,
             'category_id' => $this->category,
         ]);
-
-
-
+        
+        
+        
         if(count($this->images)){
             foreach ($this->images as $image) {
                 // $announcement->images()->create(['path' => $image->store('images', 'public')]);
                 $newFileName = "announcements/{$announcement->id}";
                 $newImage = $announcement->images()->create(['path' => $image->store($newFileName, 'public')]);
-
-                ResizeImage::dispatch($newImage->path, 300, 300);
-
-                GoogleVisionSafeSearch::dispatch($newImage->id);
-
-                GoogleVisionLabelImage::dispatch($newImage->id);
+                
+                RemoveFaces::withChain([  
+                    new ResizeImage($newImage->path, 300, 300),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id),
+                    ])->dispatch($newImage->id);
+                    
+                }
+                
+                Storage::deleteDirectory('livewire-tmp');
             }
-
-            Storage::deleteDirectory('livewire-tmp');
+            
+            $announcement->save();
+            
+            /* messaggio di errore direttamente con livewire */
+            session()->flash('message', __('ui.created'));
+            
+            $this->reset();
+            // serve per svuotare i campi
         }
-
-        $announcement->save();
-
-        /* messaggio di errore direttamente con livewire */
-        session()->flash('message', __('ui.created'));
-
-        $this->reset();
-        // serve per svuotare i campi
-    }
-
-    public function removeImage($key){
-
-        if (in_array($key, array_keys($this->images))) {
-            unset($this->images[$key]);
+        
+        public function removeImage($key){
+            
+            if (in_array($key, array_keys($this->images))) {
+                unset($this->images[$key]);
+            }
+        }
+        
+        public function render()
+        {
+            
+            $categories = Category::all();
+            return view('livewire.create-announcements', compact('categories'));
         }
     }
-
-    public function render()
-    {
-
-        $categories = Category::all();
-        return view('livewire.create-announcements', compact('categories'));
-    }
-}
-
-/*
+    
+    /*
     public $img;
     */
-
-/* if($this->img){
-            $imgPath = 'public/img/default-image.jpg';
-
-            $imgPath = $this->img->store('public/img');
-        } */
-
-
-
-        /* 'img' => $imgPath, */
+    
+    /* if($this->img){
+        $imgPath = 'public/img/default-image.jpg';
+        
+        $imgPath = $this->img->store('public/img');
+    } */
+    
+    
+    
+    /* 'img' => $imgPath, */
